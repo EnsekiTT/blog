cd712ec90600fa49ffab6f033c87579e6767493c
EnsekiTT <ensekitt@gmail.com>
2018-02-14 21:27:28 +0900

    xgBoostインストールした

---
+++ 20180211_xgboost/settings.sh
@@ -0,0 +1,2 @@
+alias clang-omp='/usr/local/opt/llvm/bin/clang -fopenmp -L/usr/local/opt/llvm/lib -Wl,-rpath,/usr/local/opt/llvm/lib'
+alias clang-omp++='/usr/local/opt/llvm/bin/clang++ -fopenmp -L/usr/local/opt/llvm/lib -Wl,-rpath,/usr/local/opt/llvm/lib'
